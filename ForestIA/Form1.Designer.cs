d45b18abdfd3e86c725945f8911262d6bcdec926
alu0100536652 <alu0100536652@ull.edu.es>
2015-10-06 23:38:41 +0100

    Pantalla inicial sin la funcionalidad

---
+++ ForestIA/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ForestIA
+{
+    partial class menu
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnSetup = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(183, 388);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(129, 28);
+            this.btnSalir.TabIndex = 0;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnSetup
+            // 
+            this.btnSetup.Location = new System.Drawing.Point(183, 354);
+            this.btnSetup.Name = "btnSetup";
+            this.btnSetup.Size = new System.Drawing.Size(129, 28);
+            this.btnSetup.TabIndex = 1;
+            this.btnSetup.Text = "Configuración";
+            this.btnSetup.UseVisualStyleBackColor = true;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(183, 320);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(129, 28);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Empezar";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnSetup);
+            this.Controls.Add(this.btnSalir);
+            this.Name = "menu";
+            this.Text = "Inicio";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnSetup;
+        private System.Windows.Forms.Button btnPlay;
+    }
+}
+
