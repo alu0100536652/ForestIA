1d8b3a3bbfecd28764f78126779a898d2d47f6ee
alu0100536652 <alu0100536652@ull.edu.es>
2015-10-07 00:09:32 +0100

    Añadiendo clase Game y Map

---
+++ ForestIA/Game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForestIA
+{
+    class Game
+    {
+
+        public Game()
+        {
+
+        }
+
+        public void Setup()
+        {
+
+        }
+
+        public void Print()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
